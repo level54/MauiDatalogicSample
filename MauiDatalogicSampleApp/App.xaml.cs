d729da1dadd4ba74519484487e112215bc515f58
level54solutions <level54.solutions@gmail.com>
2023-06-15 19:42:05 -0400

    Add project files.

---
+++ MauiDatalogicSampleApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiDatalogicSampleApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ MauiDatalogicSampleApp/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Com.Datalogic.Decode;
+using CommunityToolkit.Mvvm.Messaging;
+
+namespace MauiDatalogicSampleApp;
+
+public partial class MainPage : ContentPage, IRecipient<IDecodeResult>
+{
+	public MainPage()
+	{
+		InitializeComponent();
+        WeakReferenceMessenger.Default.Register<IDecodeResult>(this);
+    }
+
+    public void Receive(IDecodeResult message)
+    {
+        Console.WriteLine($"WeakReferenceMessenger Receive : Format={message.BarcodeID}, Value={message.Text}");
+
+        txtBarcodeText.Text = message.Text;
+		txtBarcodeID.Text = message.BarcodeID.ToString();
+    }
+}
+
